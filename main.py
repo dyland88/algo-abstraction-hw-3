da77e1e0b9f4d1021fa17ec29781b10ac8864fe5
Dylan Coben <dylan.coben@gmail.com>
2026-03-31 22:32:45 -0400

    solve max subsequence

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("Hello world")
+def solve_max_subsequence(a: list, b: list, values: dict) -> tuple[int, str]:
+    m = len(a)
+    n = len(b)
+
+    dp = [[0] * (m + 1) for i in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if a[i-1] == b[j-1]:
+                dp[i][j] = values[a[i-1]] + dp[i-1][j-1]
+            else:
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+
+    max_val = dp[n][m]
+
+    subsequence = []
+    i, j = n, m
+    while i > 0 and j > 0:
+        if a[i] == b[j]:
+            subsequence.append(a[i])
+            i -= 1
+            j -= 1
+        elif dp[i-1][j] > dp[i][j-1]:
+            i -= 1
+        else:
+            j -= 1
+
+    return max_val, "".join(reversed(subsequence))
